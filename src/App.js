383a915c6060cd5586db6f3d44fe64b799035eeb
thomas.gulli <thomas.gulli@no.experis.com>
2021-03-10 16:35:10 +0100

    Hello

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           BJØRNAR VAR HER
+          Thomas også :)
         </p>
         <a
           className="App-link"
