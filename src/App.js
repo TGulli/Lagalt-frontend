72d3cf14fcfef412433c96ccdcd25e46c483ecb7
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-03-11 10:45:50 +0100

    simple login and regiter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Login from "./components/Login/Login.js";
+import Register from "./components/Register/Register";
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          BJØRNAR VAR HER
-          Thomas også :),
-          og Camilla ^^
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </Switch>
+
+        </BrowserRouter>
+    );
 }
 
 
