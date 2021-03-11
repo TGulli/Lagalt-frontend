33d3c44b5aeab11fd968ed70ba4ecd99606d5f24
theaht <thea.hvatho@gmail.com>
2021-03-11 14:13:59 +0100

    create project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Login from "./components/Login/Login.js";
 import Register from "./components/Register/Register";
+import CreateProject from "./components/CreateProject/CreateProject"
 import './App.css';
 
 
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/project/create" component={CreateProject}/>
             </Switch>
 
         </BrowserRouter>
