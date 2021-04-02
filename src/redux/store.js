acfa2a31264d9cb1e0c40441c73e95a32b3e2dd7
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-04-02 14:48:26 +0200

    initial

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,8 @@ import rootReducer from "./reducers";
 import storage from 'redux-persist/lib/storage'
 import {persistStore, persistReducer} from "redux-persist"
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const composedEnhancer = applyMiddleware(thunkMiddleware)
 
 
 
